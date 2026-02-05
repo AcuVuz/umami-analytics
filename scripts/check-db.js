b228905bcd69958292e525ecf27d6fab24d15436
EvgenijRyabko <jeniaryabko@bk.ru>
2026-02-05 15:13:17 +0300

    return schema

--- scripts/check-db.js
+++ scripts/check-db.js
@@ -17,7 +17,7 @@ const url = new URL(process.env.DATABASE_URL);
 
 const adapter = new PrismaPg(
   { connectionString: url.toString() },
-  { schema: url.searchParams.get('public') },
+  { schema: url.searchParams.get('schema') },
 );
 
 const prisma = new PrismaClient({ adapter });
